1fb01fdd148f69f61d27c253c8a81c0c0f939722
Antoine TEUF <ateuf@octo.com>
2018-02-18 16:51:57 +0100

    Add NSubstitute and test samples

---
+++ SpotTheDefuser_Unity/Assets/Scripts/Test/Editor/FooTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using NSubstitute;
+
+public class FooTest {
+
+	[Test]
+	public void IsOk_shouldReturnTrue() {
+        Foo foo = new Foo();
+        IBar bar = Substitute.For<IBar>();
+
+        bar.IsReallyOk().Returns(true);
+
+        Assert.AreEqual(true, foo.IsOk());
+        Assert.AreEqual(true, bar.IsReallyOk());
+	}
+
+}
