601ef2fd920bc6832cfc70b0c85cfca7870903a7
TeuTeuf <antoine.teuf@gmail.com>
2018-03-08 23:38:24 +0100

    Add Zenject plugin + create PlayerController to try to use it

---
+++ SpotTheDefuser_Unity/Assets/Scripts/Main/Infrastructure/Players/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using SpotTheDefuser_Unity.Assets.Scripts.Main.Domain;
+using SpotTheDefuser_Unity.Assets.Scripts.Main.UseCases;
+using UnityEngine;
+using Zenject;
+
+public class PlayerController : MonoBehaviour {
+
+    public PlayerController prefabPlayer;
+
+    [Inject]
+    readonly DiContainer container;
+
+    [Inject]
+    readonly AddNewPlayer addNewPlayer;
+
+    [Inject]
+    readonly RemovePlayer removePlayer;
+
+    [Inject]
+    readonly GetAllPlayers getAllPlayers;
+
+    Player player;
+
+    void Start () 
+    {
+        player = new Player(gameObject.name);
+        addNewPlayer.Execute(player);
+
+        Debug.Log("New Player Added! Nb players: " + getAllPlayers.Get().Count);
+	}
+
+	void Update()
+	{
+        if(Input.GetMouseButtonDown(0)) {
+            container.InstantiatePrefab(prefabPlayer);
+        }
+	}
+
+	void OnDestroy()
+    {
+        removePlayer.execute(player);
+
+        Debug.Log("Player removed! Nb players: " + getAllPlayers.Get().Count);
+    }
+}
---
+++ SpotTheDefuser_Unity/Assets/Scripts/Main/Infrastructure/Players/PlayerInstaller.cs
@@ -0,0 +1,17 @@
+using SpotTheDefuser_Unity.Assets.Scripts.Main.Domain;
+using SpotTheDefuser_Unity.Assets.Scripts.Main.Infrastructure;
+using SpotTheDefuser_Unity.Assets.Scripts.Main.UseCases;
+using Zenject;
+
+public class PlayerInstaller : MonoInstaller<PlayerInstaller>
+{
+    public override void InstallBindings()
+    {
+        Container.Bind<IPlayersRepository>().To<LocalPlayersRepository>().AsSingle();
+
+        // Use Cases
+        Container.Bind<AddNewPlayer>().AsSingle();
+        Container.Bind<GetAllPlayers>().AsSingle();
+        Container.Bind<RemovePlayer>().AsSingle();
+    }
+}
