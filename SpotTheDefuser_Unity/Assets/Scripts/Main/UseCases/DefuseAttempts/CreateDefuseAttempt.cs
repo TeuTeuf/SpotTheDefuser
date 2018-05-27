b145561ce3ba515a87b0848c956363ae8f4a1401
TeuTeuf <antoine.teuf@gmail.com>
2018-05-27 17:09:02 +0200

    Add CreateDefuseAttempt use case and corresponding test

---
+++ SpotTheDefuser_Unity/Assets/Scripts/Main/UseCases/DefuseAttempts/CreateDefuseAttempt.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Main.Domain.DefuseAttempts;
+
+namespace Main.UseCases.DefuseAttempts
+{
+	public class CreateDefuseAttempt {
+
+		public DefuseAttempt Execute()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ SpotTheDefuser_Unity/Assets/Scripts/Test/Editor/UseCases/DefuseAttempts/CreateDefuseAttemptTest.cs
@@ -0,0 +1,22 @@
+﻿using Main.Domain.DefuseAttempts;
+using Main.UseCases.DefuseAttempts;
+using NUnit.Framework;
+
+namespace Test.Editor.UseCases.DefuseAttempts
+{
+    public class CreateDefuseAttemptTest {
+
+        [Test]
+        public void Execute_ShouldReturnADefuseAttempt()
+        {
+            // Given
+            var createDefuseAttempt = new CreateDefuseAttempt();
+            
+            // When
+            var defuseAttempt = createDefuseAttempt.Execute();
+
+            // Then
+            Assert.IsInstanceOf<DefuseAttempt>(defuseAttempt);
+        }
+    }
+}
