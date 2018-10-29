a084d70f9f3b8ffca0578125b5e81fa344365ceb
TeuTeuf <antoine.teuf@gmail.com>
2018-10-29 22:42:37 +0100

    Add Lobby, Defusing, End layers + debug buttons

---
+++ Assets/Scripts/DebugViewLayers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Main.Domain.UI;
+using Main.UseCases.UI;
+using UnityEngine;
+using Zenject;
+
+public class DebugViewLayers : MonoBehaviour {
+
+    private ChangeCurrentView _changeCurrentView;
+
+    [Inject]
+    public void Init(ChangeCurrentView changeCurrentView)
+    {
+        _changeCurrentView = changeCurrentView;
+    }
+    
+    public void ChangeView(View view)
+    {
+        _changeCurrentView.Change(view);
+    }
+    
+    public void ChangeView(string strView)
+    {
+        View view;
+        Enum.TryParse(strView, out view);
+        _changeCurrentView.Change(view);
+    }
+}
