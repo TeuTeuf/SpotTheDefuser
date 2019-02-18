4dde99ff4254016f37c39d589778ce4ee96c7ee0
TeuTeuf <antoine.teuf@gmail.com>
2019-02-18 08:43:56 +0100

    Set default number of defusers and bombs on Lobby Layer and increment number of defusers when player connect

--- Assets/Scripts/Main/Infrastructure/UI/LobbyLayer.cs
+++ Assets/Scripts/Main/Infrastructure/UI/LobbyLayer.cs
@@ -1,18 +1,32 @@
 using Main.Domain.Players;
 using Main.Domain.UI;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
 
 namespace Main.Infrastructure.UI
 {
     public class LobbyLayer : BaseLayer, ILobbyLayer
     {
-        public override View GetView()
+        [FormerlySerializedAs("NbDefusersText")] public Text nbDefusersText;
+        [FormerlySerializedAs("NbBombsText")] public Text nbBombsText;
+
+        private const string DEFAULT_NB_DEFUSERS_DISPLAYED = "1";
+        private const string DEFAULT_NB_BOMBS_DISPLAYED = "0";
+
+        public void Start()
         {
-            return View.Lobby;
+            nbDefusersText.text = DEFAULT_NB_DEFUSERS_DISPLAYED;
+            nbBombsText.text = DEFAULT_NB_BOMBS_DISPLAYED;
         }
 
         public void UpdatePlayerList(Player[] allPlayers)
         {
-            throw new System.NotImplementedException();
+            nbDefusersText.text = allPlayers.Length.ToString();
+        }
+
+        public override View GetView()
+        {
+            return View.Lobby;
         }
     }
 }
--- Assets/Scripts/Test/TestsEditMode/Infrastructure/UI/LobbyLayerTest.cs
+++ Assets/Scripts/Test/TestsEditMode/Infrastructure/UI/LobbyLayerTest.cs
@@ -2,6 +2,7 @@ using Main.Domain.UI;
 using Main.Infrastructure.UI;
 using NUnit.Framework;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Test.TestsEditMode.Infrastructure.UI
 {
@@ -10,11 +11,23 @@ namespace Test.TestsEditMode.Infrastructure.UI
     {
         private LobbyLayer _lobbyLayer;
 
-
         [SetUp]
         public void Init()
         {
             _lobbyLayer = new GameObject().AddComponent<LobbyLayer>();
+            _lobbyLayer.nbDefusersText = new GameObject().AddComponent<Text>();
+            _lobbyLayer.nbBombsText = new GameObject().AddComponent<Text>();
+        }
+
+        [Test]
+        public void Start_ShouldSetNbDefusersTo1AndBombsTo0()
+        {
+            // When
+            _lobbyLayer.Start();
+
+            // Then
+            Assert.That(_lobbyLayer.nbDefusersText.text, Is.EqualTo("1"));
+            Assert.That(_lobbyLayer.nbBombsText.text, Is.EqualTo("0"));
         }
         
         [Test]
