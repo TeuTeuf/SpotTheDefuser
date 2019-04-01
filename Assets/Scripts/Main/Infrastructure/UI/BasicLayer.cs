31de9f8ce09cc919b7f74ee396a2be9c4060bb96
TeuTeuf <antoine.teuf@gmail.com>
2019-04-01 08:25:57 +0200

    Remove useless FormerlySerializedAs...

--- Assets/Scripts/Main/Infrastructure/UI/BasicLayer.cs
+++ Assets/Scripts/Main/Infrastructure/UI/BasicLayer.cs
@@ -5,7 +5,7 @@ namespace Main.Infrastructure.UI
 {
     public class BasicLayer : BaseLayer
     {
-        [FormerlySerializedAs("View")] public View view;
+        public View view;
 
         public override View GetView()
         {
